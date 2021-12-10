25f6dd3373ab1e929056c4cc504fd86c76a84476
iCarambaaa <dkpress1916@zoho.eu>
2021-12-10 10:45:29 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express from "express"
-import cors from "cors"
-import  listEndpoints from "express-list-endpoints"
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
+process.env.TS_NODE_DEV && require("dotenv").config();
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
-console.table(listEndpoints(server))
+console.table(listEndpoints(server));
 
-export default server
+export default server;
